ae4d57e5306b5aaf0b4f79c50fb3cf78c51ddb93
aydanmaliki <amaliki17577@ada.edu.az>
2025-05-27 14:45:42 +0400

    Added Pomodoro scene , pomodoro time selection , and the timer. added Level selection , made changes to the buttons, and level selection logic(for ex:finish lev1 to get to lev2)

--- Flow_Flourish/Assets/Scripts/ManageObjects.cs
+++ Flow_Flourish/Assets/Scripts/ManageObjects.cs
@@ -1,24 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ManageObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void StartGame(){
-
-        SceneManager.LoadScene("PipeWithAutoGenerateGrid");
+        // Load Level Selection instead of the game directly
+        SceneManager.LoadScene("LevelSelection");
     }
 }
